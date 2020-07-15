23721392c76f87dbf2e0b57ac72f770a679fd84f
nahuel-ojeda <nahuelo2222@hotmail.com>
2020-07-15 13:19:58 -0300

    qwer

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+const express = require('express');
+const app = express();
+
+app.listen(3030, () => console.log('servidor funcionando en el puerto 3030'))
+
+const routesAutos = require('./routes/autos');
+const routesHome = require('./routes/home');
+const routesSucursales = require('./routes/sucursales');
+const routesMarcas = require('./routes/marcas');
+
+app.use('/', routesHome);
+app.use('/sucursales', routesSucursales);
+app.use('/marcas', routesMarcas);
+app.use('/autos', routesAutos);
